6406cef1d59b092af5cb918ceabdf4888acac565
mikeleo03 <leonmichael463@gmail.com>
2024-07-23 10:36:15 +0700

    [Init] Initiate project

---
+++ fpt_midterm_pos/src/main/java/com/example/fpt_midterm_pos/FptMidtermPosApplication.java
@@ -0,0 +1,13 @@
+package com.example.fpt_midterm_pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FptMidtermPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FptMidtermPosApplication.class, args);
+	}
+
+}
