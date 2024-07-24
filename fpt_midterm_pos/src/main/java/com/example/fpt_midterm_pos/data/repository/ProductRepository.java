3586f4cd008885b374b79801ea15aaa6820ea0c6
mikeleo03 <leonmichael463@gmail.com>
2024-07-24 11:43:53 +0700

    [Feat] set ID to UUID

--- fpt_midterm_pos/src/main/java/com/example/fpt_midterm_pos/data/repository/ProductRepository.java
+++ fpt_midterm_pos/src/main/java/com/example/fpt_midterm_pos/data/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.example.fpt_midterm_pos.data.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.fpt_midterm_pos.data.model.Product;
 import com.example.fpt_midterm_pos.data.model.Status;
 
-public interface ProductRepository extends JpaRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
     List<Product> findByNameContainingAndStatus(String name, Status status);
     Page<Product> findAllByStatus(Status status, Pageable pageable);
     Page<Product> findByStatusAndNameContaining(Status status, String name, Pageable pageable);
