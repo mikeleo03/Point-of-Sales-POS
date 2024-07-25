334b24bed26d428a27deeafcda5b9314bd80379a
Fadhly Al-farizi <fadhlyalfarizi30@gmail.com>
2024-07-25 19:22:42 +0700

    [Refactor] Remove Unused Import

--- fpt_midterm_pos/src/main/java/com/example/fpt_midterm_pos/data/repository/InvoiceRepository.java
+++ fpt_midterm_pos/src/main/java/com/example/fpt_midterm_pos/data/repository/InvoiceRepository.java
@@ -3,7 +3,6 @@ package com.example.fpt_midterm_pos.data.repository;
 import java.util.Date;
 import java.util.UUID;
 
-import org.apache.poi.hpsf.Decimal;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
--- fpt_midterm_pos/src/main/java/com/example/fpt_midterm_pos/service/impl/InvoiceServiceImpl.java
+++ fpt_midterm_pos/src/main/java/com/example/fpt_midterm_pos/service/impl/InvoiceServiceImpl.java
@@ -10,9 +10,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.Calendar;
 
-import org.apache.poi.hpsf.Decimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
