068e2a3fb768f7ee7a0597d93fa7a3da8e776c4f
AliRamadhan <aliramdhan4551@gmail.com>
2024-08-29 22:13:57 +0700

    feat : implement server to client

--- fpt_midterm_pos/src/main/java/com/example/fpt_midterm_pos/config/filter/JwtRequestFilter.java
+++ fpt_midterm_pos/src/main/java/com/example/fpt_midterm_pos/config/filter/JwtRequestFilter.java
@@ -36,11 +36,20 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         String requestURI = request.getRequestURI();
-        
-        logger.info("[JWTFilter][" + request + "]" + "[" + request.getMethod()+ "] " + requestURI);
+
+        logger.info("[JWTFilter][" + request + "]" + "[" + request.getMethod() + "] " + requestURI);
 
         // Exclude the /api/v1/authentication endpoint and any nested routes
         if (requestURI.startsWith("/api/v1/authentication")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (request.getMethod().equals("GET") ||
+                request.getMethod().equals("POST") ||
+                request.getMethod().equals("PUT") ||
+                request.getMethod().equals("DELETE") ||
+                request.getMethod().equals("OPTIONS")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -88,4 +97,3 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         logger.info("[JWTFilter] Logging Response : {}", response.getStatus());
     }
 }
-
