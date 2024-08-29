5402827294a5bfb23e4f124e05514a4db823aa80
AliRamadhan <aliramdhan4551@gmail.com>
2024-08-29 14:57:44 +0700

    chore : update modal invoice ui

--- standalone-app/src/app/pages/invoices/invoice-modal/invoice-modal.component.ts
+++ standalone-app/src/app/pages/invoices/invoice-modal/invoice-modal.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { PriceFormatPipe } from '../../../core/pipes/price-format.pipe';
+import { DateFormatPipe } from '../../../core/pipes/date-format.pipe';
 @Component({
   selector: 'app-invoice-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PriceFormatPipe, DateFormatPipe],
   templateUrl: './invoice-modal.component.html',
   styleUrl: './invoice-modal.component.css',
 })
